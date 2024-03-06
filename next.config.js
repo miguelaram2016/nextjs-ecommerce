457d92e110b85d27cdb9c773e70dd1759cd71e69
miguelaram2016 <miguelaram2016@utexas.edu>
2024-03-06 00:55:52 -0600

    Getting closer

--- next.config.js
+++ next.config.js
@@ -56,13 +56,13 @@ const nextConfig = {
     // and configure the admin route to your Payload CMS.
 
     // Point to your Payload config (required)
-    configPath: path.resolve(__dirname, './src/payload/payload.config.ts'),
+    configPath: path.resolve(__dirname, './payload/payload.config.ts'),
 
     // Point to custom Payload CSS (optional)
-    cssPath: path.resolve(__dirname, './src/app/_css/my-custom-payload-styles.css'),
+    cssPath: path.resolve(__dirname, './css/my-custom-payload-styles.css'),
 
     // Point to your exported, initialized Payload instance (optional, default shown below`)
-    payloadPath: path.resolve(process.cwd(), './src/payload/payloadClient.ts'),
+    payloadPath: path.resolve(process.cwd(), './payload/payloadClient.ts'),
 
     // Set a custom Payload admin route (optional, default is `/admin`)
     // NOTE: Read the "Set a custom admin route" section in the payload/next-payload README.
